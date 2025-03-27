d22c1f35dcb190749c1b2affd789b917cd384ae2
feliperj629 <feliperj629@gmail.com>
2025-03-26 21:26:21 -0300

    Criando novos projetos

--- aula1/js/script.js
+++ aula1/js/script.js
@@ -1,10 +1,12 @@
 function limparFormulario() {
     document.getElementById('cadastroForm').reset();
+    alert('Formulário limpo com sucesso!');
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('cadastroForm').addEventListener('submit', function (e) {
-        e.preventDefault();
-        alert('Dados salvos com sucesso!');
-    });
-}); 
+document.addEventListener('DOMContentLoaded',
+    function () {
+        document.getElementById('cadastroForm').addEventListener('submit', function (e) {
+            e.preventDefault();
+            alert('Dados salvos com sucesso!');
+        });
+    }); 
